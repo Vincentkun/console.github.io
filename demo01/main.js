c94b0ff91cea603e717e75fb7192b5a9ebe2771c
Frank <lee0324@126.com>
2017-07-04 19:13:10 +0800

    :globe_with_meridians: demo :bulb:

--- demo01/main.js
+++ demo01/main.js
@@ -1,9 +1,16 @@
 console.log("main.js start");
+
+// 主线程先启动一个worker子线程
 var worker = new Worker("worker.js");
  
+
+// 同时监听onmessage，取到子线程给它传递的计算结果
 worker.onmessage = function(event){
     console.log("recieve result: " + event.data);
 };
  
-var num = 1000;
+// init number 
+var num = 10;
+
+// 把数据发给worker
 worker.postMessage(num);
--- demo01/worker.js
+++ demo01/worker.js
@@ -1,3 +1,7 @@
+// 斐波那契数列：
+// 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...
+
+// 斐波那契函数
 function fibonacci(num) {
 	if (num <= 0) return 0;
 	if (num === 1 || num === 2) return 1;
@@ -12,7 +16,12 @@ function fibonacci(num) {
 	return fn2;
 };
 
+//我们先打印出 wokderjs 开始
 console.log('worker.js start');
+
+// 把这个fibonacci函数写到worker.js里面
+//webworker有一个全局的函数叫onmessage
+//在这个回调里面监听接收主线程的数据
 onmessage = function(event) {
 	//主线程的数据通过event.data传进来
 	var num = event.data;
@@ -20,3 +29,5 @@ onmessage = function(event) {
 	//计算完结果，给主线程发送一个消息
 	postMessage(result);
 };
+
+// 计算完结果后，再把结果postMessage给主线程
